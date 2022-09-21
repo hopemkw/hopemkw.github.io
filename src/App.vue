119f18a1ecfe7b859848c1e9645927e0a072c985
joe <wf_pinbo@163.com>
2022-09-21 14:34:34 +0800

    vue init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <a href="https://pan.baidu.com/s/1DtHSgoEyL2__mp3IvO4-tg?pwd=6666">mac</a>
+  <a href="https://pan.baidu.com/s/1RfJOfb4uelMNuU-5Hot-DA?pwd=6666">android</a>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
